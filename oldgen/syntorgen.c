884dacce48c36814ba669a107d2ab075506c2afa
drb <drb>
2002-07-24 03:21:07 +0000

    Final cleanup before initial public pre-release.

--- oldgen/syntorgen.c
+++ oldgen/syntorgen.c
@@ -2,6 +2,22 @@
    Primitive program to generate a code plug for a high-band Syntor X
    Copyright (C) 2002, Dennis Boone, East Lansing, MI */
 
+/* This file is part of syntorxgen.
+   
+   Syntorxgen is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   Syntorxgen is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with syntorxgen; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+
 #include <stdio.h>
 
 #define MAXSTR        255
--- utils/testmemold.pl
+++ utils/testmemold.pl
@@ -2,6 +2,22 @@
 # testmemold.pl, Boone, 07/23/02
 # Generate an old style syntorgen input file for a "test ROM" style code plug
 # Copyright (C) 2002, Dennis Boone, East Lansing, MI
+#
+# This file is part of syntorxgen.
+# 
+# Syntorxgen is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Syntorxgen is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with syntorxgen; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 for ($i = 0; $i < 32; $i++)
 {
